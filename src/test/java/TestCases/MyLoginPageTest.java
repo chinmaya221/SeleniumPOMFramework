a87bbd83d65e026f7e2c9ff97cc94542921ae489
ckdeo <ckdeo@LAPTOP-BUA0KBG0>
2021-12-03 19:00:25 +0530

    This is the commit from ckdeo

---
+++ src/test/java/TestCases/MyLoginPageTest.java
@@ -0,0 +1,13 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class MyLoginPageTest {
+	
+	
+	@Test
+	public void doLogin() {
+		System.out.println("This is just a test login");
+	}
+
+}
